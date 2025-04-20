4e02cffb7247015863658c74c67160af971c44f2
DanielaOdileth <odilethpadilla@unitec.edu>
2025-04-19 20:42:52 -0600

    adding price input logic

--- src/app/components/Form/index.tsx
+++ src/app/components/Form/index.tsx
@@ -5,9 +5,11 @@ import { Select } from "../Select";
 
 export function Form() {
   const [symbolSelected, setSymbolSelected] = useState<string>("");
+  const [priceAlert, setPriceAlert] = useState<string | number>("");
 
   const onSubmit = () => {
-    console.log("onSubmit ", symbolSelected);
+    setPriceAlert("");
+    setSymbolSelected("");
   };
 
   return (
@@ -23,7 +25,6 @@ export function Form() {
           value={symbolSelected}
         />
       </div>
-
       <div className="flex mt-5">
         <label htmlFor="price" className="mr-10">
           Price:
@@ -33,12 +34,15 @@ export function Form() {
           className="w-32 px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:border-gray-500"
           step="0.01"
           min="0"
+          onChange={(e) => setPriceAlert(Number(e.target.value))}
+          value={priceAlert}
         />
       </div>
       <div className="flex justify-end">
         <button
           className="border-blue-400 disabled:bg-gray-900 disabled:cursor-not-allowed disabled:text-black disabled:opacity-70 w-36 border-2 rounded-sm p-2 mt-10 font-semibold hover:bg-blue-100"
           onClick={onSubmit}
+          disabled={!symbolSelected || !priceAlert}
         >
           Save
         </button>
